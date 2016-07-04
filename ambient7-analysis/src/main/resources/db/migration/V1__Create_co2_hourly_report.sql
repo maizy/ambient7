9b1e5b728dc5c610a0f3b94989e5d83a7eda918f
Nikita Kovaliov <nikita@maizy.ru>
2016-07-04 10:00:19 +0300

    iss #18: analysis: add tags & agent name as aggregate results columns

--- ambient7-analysis/src/main/resources/db/migration/V1__Create_co2_hourly_report.sql
+++ ambient7-analysis/src/main/resources/db/migration/V1__Create_co2_hourly_report.sql
@@ -1,10 +1,15 @@
 create table "co2_hourly_report" (
   co2_hourly_report_id identity primary key not null,
-  day date not null ,
-  hour smallint not null,
-  low_level_total int null,
-  middle_level_total int null,
-  high_level_total int null
+
+  day                  date                 not null,
+  hour                 smallint             not null,
+  tags_ordered         varchar(3072)        not null,
+  agent_name           varchar(255)         not null,
+
+  low_level_total      int                  null,
+  middle_level_total   int                  null,
+  high_level_total     int                  null
 );
 
-create unique index on "co2_hourly_report" (day, hour);
+create unique index "unique_record" on "co2_hourly_report" (day, hour, tags_ordered, agent_name);
+create index "aggregate_ident" on "co2_hourly_report" (tags_ordered, agent_name);
