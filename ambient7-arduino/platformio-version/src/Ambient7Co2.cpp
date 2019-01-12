0df6f2d478c93e679f5167fc1c70a7db9557aacb
Nikita Kovaliov <nikita@maizy.ru>
2019-01-12 16:57:32 +0300

    iss #46: read DHT22 sensor data

--- ambient7-arduino/platformio-version/src/Ambient7Co2.cpp
+++ ambient7-arduino/platformio-version/src/Ambient7Co2.cpp
@@ -22,19 +22,19 @@ int readCO2UART(SoftwareSerial co2Serial, unsigned int timeoutMillis = 5000)
     byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
     byte response[9]; // for answer
 
-    Serial.println("Sending CO2 request...");
+    // Serial.println("Sending CO2 request...");
     co2Serial.write(cmd, 9); //request PPM CO2
 
     // clear the buffer
     memset(response, 0, 9);
 
-    int elapsed = 0;
-    int delayMillis = 500;
+    unsigned int elapsed = 0;
+    unsigned int delayMillis = 500;
     while (co2Serial.available() == 0) {
         delay(delayMillis);
         elapsed += delayMillis;
         if (elapsed > timeoutMillis) {
-            Serial.println("ERROR: Unable to get Co2 data");
+            // Serial.println("ERROR: Unable to get Co2 data");
             return -1;
         }
     }
@@ -44,33 +44,33 @@ int readCO2UART(SoftwareSerial co2Serial, unsigned int timeoutMillis = 5000)
     }
 
     // print out the response in hex
-    for (int i = 0; i < 9; i++) {
-    Serial.print(String(response[i], HEX));
-        Serial.print("   ");
-    }
-    Serial.println("");
+    // for (int i = 0; i < 9; i++) {
+    // Serial.print(String(response[i], HEX));
+    //     Serial.print("   ");
+    // }
+    // Serial.println("");
 
     // checksum
     byte check = countCheckSum(response);
     if (response[8] != check) {
-        Serial.println("Checksum not OK!");
-        Serial.print("Received: ");
-        Serial.println(response[8]);
-        Serial.print("Should be: ");
-        Serial.println(check);
+        // Serial.println("Checksum not OK!");
+        // Serial.print("Received: ");
+        // Serial.println(response[8]);
+        // Serial.print("Should be: ");
+        // Serial.println(check);
         return -2;
     }
 
     // ppm
     int ppm_uart = 256 * (int)response[2] + response[3];
-    Serial.print("PPM: "); Serial.println(ppm_uart);
+    //Serial.print("PPM: "); Serial.println(ppm_uart);
 
-    // status
-    byte status = response[5];
-    Serial.print("Status: "); Serial.println(String(status, HEX));
-    if (status == 0x40) {
-        Serial.println("Status OK");
-    }
+    // TODO: check status
+    // byte status = response[5];
+    // Serial.print("Status: "); Serial.println(String(status, HEX));
+    // if (status == 0x40) {
+    //     Serial.println("Status OK");
+    // }
 
     return ppm_uart;
 }
--- ambient7-arduino/platformio-version/src/Ambient7Wifi.cpp
+++ ambient7-arduino/platformio-version/src/Ambient7Wifi.cpp
@@ -6,14 +6,13 @@ void setupWifi(const char* network, const char* password)
 {
     WiFi.mode(WIFI_STA);
     WiFi.begin(network, password);
-    Serial.print("Connecting to wifi: ");
+    Serial.print("Connecting to wifi: ...");
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-    Serial.println(" done");
-
-    Serial.println("Connected to WIFI network");
-    Serial.print("IP address: "); Serial.println(WiFi.localIP());
+    Serial.print(" done  IP: "); Serial.print(WiFi.localIP());
+    Serial.print(" Gateway: "); Serial.println(WiFi.gatewayIP());
+    Serial.println("");
 }
