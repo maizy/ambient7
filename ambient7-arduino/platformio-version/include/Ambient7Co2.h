fee46c25331d8e999c0f6d1f1ddfc917369c1795
Nikita Kovaliov <nikita@maizy.ru>
2019-01-20 19:56:21 +0300

    iss #46: fix MH-Z19 reading error

--- ambient7-arduino/platformio-version/include/Ambient7Co2.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <SoftwareSerial.h>
-
-extern int readCO2UART(SoftwareSerial co2Serial, unsigned int timeoutMillis = 5000);
--- ambient7-arduino/platformio-version/src/main.cpp
+++ ambient7-arduino/platformio-version/src/main.cpp
@@ -4,18 +4,18 @@
 //#include <ESP8266mDNS.h>
 #include "configs.h"
 #include "Ambient7Wifi.h"
-#include "Ambient7Co2.h"
-#include "DHT.h"
+#include <DHT.h>
+#include <MHZ19_uart.h>
 
 
 unsigned long startTime = millis();
 
-SoftwareSerial co2Serial(CO2_RX, CO2_TX);
+MHZ19_uart mhz19;
 DHT dht;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println(""); Serial.println("");
   Serial.println("Ambient7");
   Serial.println("https://github.com/maizy/ambient7/");
@@ -24,10 +24,13 @@ void setup()
   Serial.print("  CO2 RX Pin: "); Serial.println(CO2_RX);
   Serial.print("  CO2 TX Pin: "); Serial.println(CO2_TX);
   Serial.print("  DHT22 1wire Pin: "); Serial.println(DTH22_1WIRE);
-  Serial.print("  Wifi network: "); Serial.println(WIFI_NETWORK);
-  Serial.print("  Wifi password: ");
-  for (unsigned int i = 0; i < strlen(WIFI_PASSWORD); i++) {
-    Serial.print('*');
+  Serial.print("  Wifi Client: "); Serial.println(ENABLE_WIFI_CLIENT ? "enabled" : "disabled");
+  if (ENABLE_WIFI_CLIENT) {
+    Serial.print("  Wifi network: "); Serial.println(WIFI_NETWORK);
+    Serial.print("  Wifi password: ");
+    for (unsigned int i = 0; i < strlen(WIFI_PASSWORD); i++) {
+      Serial.print('*');
+    }
   }
   Serial.println("");
   Serial.println("");
@@ -36,15 +39,24 @@ void setup()
   dht.setup(DTH22_1WIRE);
   Serial.println(" done");
 
-  setupWifi(WIFI_NETWORK, WIFI_PASSWORD);
-  Serial.println("");
+  Serial.println("Setup MH-Z19 ...");
+  mhz19.begin(CO2_RX, CO2_TX);
+  mhz19.setAutoCalibration(false);
+  Serial.println("Done");
+
+  if (ENABLE_WIFI_CLIENT) {
+    setupWifi(WIFI_NETWORK, WIFI_PASSWORD);
+    Serial.println("");
+  }
 }
 
 void loop()
 {
   Serial.print("Uptime: "); Serial.print((millis() - startTime) / 1000); Serial.println(" s");
 
-  int co2Ppm = readCO2UART(co2Serial, 1000);
+  Serial.print("MH-Z19 status: "); Serial.print(mhz19.getStatus());
+  Serial.print(" warming: "); Serial.println(mhz19.isWarming() ? "true" : "false");
+  int co2Ppm = mhz19.getPPM();
   if (co2Ppm < 0) {
     Serial.println("ERROR: failed to read from Co2 sensor");
   } else {
