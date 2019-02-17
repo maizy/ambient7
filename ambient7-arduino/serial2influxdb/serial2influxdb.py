723f9b578bf11b33b444a5b697bc44a868f206d4
Nikita Kovaliov <nikita@maizy.ru>
2019-02-17 09:15:39 +0300

    iss #47: send data to influxdb

--- ambient7-arduino/serial2influxdb/serial2influxdb.py
+++ ambient7-arduino/serial2influxdb/serial2influxdb.py
@@ -4,8 +4,11 @@ import argparse
 import configparser
 import logging
 import time
+import re
+import datetime
 
 import serial
+import influxdb
 
 
 SERIAL_RETRY_DELAY = 5.0
@@ -31,7 +34,8 @@ def open_serial(config):
             return serial.Serial(
                 port=config['serial']['tty'],
                 baudrate=int(config['serial']['baud']),
-                timeout=1
+                timeout=1,
+                exclusive=True
             )
         except serial.SerialException as e:
             logger.warning('unable to open pyserial connection: {}'.format(e))
@@ -60,8 +64,44 @@ def resilient_line_generator(config):
                 pass
 
 
+def collect_data(key, value, tags=None):
+    data = {
+        'time': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'measurement': key,
+    }
+    if tags:
+        data['tags'] = tags
+    if key == 'uptime':
+        data['fields'] = {'value': int(value.rstrip('s'))}
+    elif key == 'humidity':
+        data['fields'] = {'value': float(value.strip('%'))}
+    elif key == 'co2':
+        if value.endswith('PPM'):
+            value = value[:-3]
+        data['fields'] = {'value': int(value)}
+    elif key == 'temperature':
+        data['fields'] = {'value': float(value.strip('C'))}
+    else:
+        return None
+    return [data]
+
+
+def build_influxdb_client(config):
+    opts = {
+        'host': config['influxdb']['server'],
+        'port': int(config['influxdb']['port']),
+        'database': config['influxdb']['database']
+    }
+    if 'username' in config['influxdb']:
+        opts['username'] = config['influxdb']['username']
+        opts['password'] = config['influxdb']['password']
+    return influxdb.InfluxDBClient(**opts)
+
+
 def main(args):
     cli_args, config = parse_args_and_config(args)
+
+    influxdb_client = build_influxdb_client(config)
 
     logging.basicConfig(
         level=logging.DEBUG if cli_args.verbose else logging.INFO,
@@ -69,9 +109,27 @@ def main(args):
         format='%(asctime)s %(levelname).1s %(message)s'
     )
 
-    for line in resilient_line_generator(config):
-        logger.debug(line)
+    tags = {}
+    if 'metrics' in config and 'tags' in config['metrics']:
+        for pair in config['metrics']['tags'].split(','):
+            tag_key, _, tag_value = pair.partition('=')
+            tags[tag_key] = tag_value
 
+    try:
+        for line in resilient_line_generator(config):
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug("receive line: %r", line)
+            data_match = re.match(r'DATA: (?P<key>[a-z0-9_]+)=(?P<value>.+)', line, re.IGNORECASE)
+            if data_match is not None:
+                key = data_match.group('key')
+                raw_value = data_match.group('value')
+                logging.info('%s=%s', key, raw_value)
+
+                data = collect_data(key, raw_value, tags)
+                if data is not None:
+                    influxdb_client.write_points(data)
+    except KeyboardInterrupt:
+        return 1
     return 0
 
 
